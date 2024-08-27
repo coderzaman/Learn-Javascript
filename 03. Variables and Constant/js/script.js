5f3b8c2d31c8841bceae0d03545d87f4fab95e98
coderzaman <coder.aktar@gmail.com>
2024-08-27 22:09:46 +0600

    variable and constant

---
+++ 03. Variables and Constant/js/script.js
@@ -0,0 +1,27 @@
+//Declare variables
+var var1 = 10;
+console.log(var1);
+
+//Reassign variables
+var var2 = 10;
+var2 = 32;
+console.log(var2);
+
+//Assign variables in another variable
+var2 = var1;
+console.log(var1);
+
+// Declare const
+const CONS = 1;
+console.log(CONS);
+
+//Incorrect declaration constant
+// const NUM;
+// NUM = 1;
+
+
+var a;
+var b = a;
+a =34;
+
+console.log(b);
