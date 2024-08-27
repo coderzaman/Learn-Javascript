74d26eb21cc9508d3481aa870929395dd5595cb1
coderzaman <coder.aktar@gmail.com>
2024-08-27 21:14:06 +0600

    start-coding

---
+++ 02. Start Coding/js/script.js
@@ -0,0 +1,24 @@
+// print a MessageChannel(Show output)
+console.log("Bimillahir Rahmanir Rahim");
+
+console.log("starting code");
+
+//we can not using same qutation in another qutation for printing
+console.log("hello, 'Bnaglades'");
+console.log('hello, "Bnaglades"');
+
+// Resulting an error
+// console.log("hello, "Bangladesh"")
+
+/*Multiline 
+Comment*/
+
+// single line Comment
+
+
+// JavaScript User Input
+// Use prompt function for user input
+// Declare variables
+var x;
+x = prompt("Enter Your Name:");
+console.log(x);
