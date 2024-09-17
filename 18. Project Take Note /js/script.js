0273c393f1477f8e973fe4e9e8456fa10686aa39
coderzaman <coder.aktar@gmail.com>
2024-09-17 23:13:30 +0600

    Project-01 Complte and API

--- 18. Project Take Note /js/script.js
+++ 18. Project Take Note /js/script.js
@@ -2,7 +2,7 @@ let titleInput = document.getElementById('noteTitle');
 let noteInput = document.getElementById('noteText');
 let noteTakeBtn = document.getElementById('noteTakeBtn');
 let NotesDiv = document.querySelector('.notes');
-let allNotes = document.querySelectorAll('.notes');
+let allNotes = document.querySelector('.notes');
 let deletAll = document.getElementById('del_all');
 let filterInput = document.getElementById('filter_task');
 
@@ -11,7 +11,7 @@ noteTakeBtn.addEventListener('click', takeNote);
 NotesDiv.addEventListener('click', removeNote);
 deletAll.addEventListener('click', deletAllNotes);
 filterInput.addEventListener('keyup', filterTask);
-
+document.addEventListener('DOMContentLoaded', getNotes);
 
 
 function takeNote(e) {
@@ -33,16 +33,23 @@ function removeNote(e) {
         if (confirm('Are you sure you want to remove')) {
             let delteNote = e.target.parentElement.parentElement;
             delteNote.remove();
+            removeFromLs(delteNote);
         }
     }
 }
 
 function deletAllNotes(e) {
     if (confirm("Are you Want to Delete All Notes?")) {
+        
         while (allNotes.firstChild) {
+            console.log('Deleting all notes');
+            console.log(allNotes.firstChild);
             allNotes.removeChild(allNotes.firstChild);
         }
+
+        localStorage.clear();
     }
+   
 }
 
 
@@ -82,6 +89,11 @@ function createNoteElement(title, description) {
 
 
     NotesDiv.appendChild(createNoteDiv);
+
+    storeInLocalStorage(title, description);
+    titleInput.value = '';
+    noteInput.value = '';
+
 }
 
 
@@ -99,6 +111,96 @@ function filterTask(e) {
         }
 
     });
+}
 
+function storeInLocalStorage(title, description){
+    let tasks;
+    let note={};
+    note.title = title;
+    note.description = description;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(note);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+function fetchDataElement(title, description) {
+    let createNoteDiv = document.createElement('div');
+    createNoteDiv.classList = "note shadow p-3 mb-1 text-bg-secondary";
+
+    let createNoteTitle = document.createElement('div');
+    createNoteTitle.classList = "note_title";
+
+    let noteHeading = document.createElement('h4');
+    noteHeading.style.display = "inline-block";
+    noteHeading.appendChild(document.createTextNode(title));
+
+    let deleteAncorBtn = document.createElement('a');
+    deleteAncorBtn.setAttribute('href', '#');
+    deleteAncorBtn.style.float = 'right';
+    deleteAncorBtn.classList = "btn btn-danger"
+    deleteAncorBtn.appendChild(document.createTextNode('Delete'));
+
+    createNoteTitle.appendChild(noteHeading);
+    createNoteTitle.appendChild(deleteAncorBtn);
+
+    let dividerHr = document.createElement('hr');
+
+    let noteDescp = document.createElement('div');
+    noteDescp.classList = "noteDesc";
+
+    let noteTextPara = document.createElement('p');
+    noteTextPara.appendChild(document.createTextNode(description));
+    noteDescp.appendChild(noteTextPara);
+
+
+    createNoteDiv.appendChild(createNoteTitle);
+    createNoteDiv.appendChild(dividerHr);
+    createNoteDiv.appendChild(noteDescp);
+
+
+    NotesDiv.appendChild(createNoteDiv);
+
+}
+
+function getNotes(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(task=>{
+        // console.log(task.title, task.description);
+       fetchDataElement(task.title, task.description);
+    })
+}
+
+
+function removeFromLs(delteNote){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    let note = delteNote.firstChild.firstChild.textContent;
+
+    tasks.forEach((item, index)=>{
+        if(item.title === note){
+            tasks.splice(index, 1);
+        }
+     })
+
+     localStorage.setItem('tasks', JSON.stringify(tasks));
 
 }
--- 19. Web Api/Storage API/js/script.js
+++ 19. Web Api/Storage API/js/script.js
@@ -9,3 +9,52 @@
 // Session Storage:
 // The localStorage.setItem() method stores a data item in a storage.
 // It takes a name and a value as parameters:
+
+
+// Storage API 
+// The localStorage Object
+// The localStorage object provides access to a local storage for a particular Web Site. It allows you to store, read, add, modify, and delete data items for that domain.
+// The data is stored with no expiration date, and will not be deleted when the browser is closed.
+// The data will be available for days, weeks, and years.
+
+
+let email = document.getElementById('email');
+let uName = document.getElementById('name');
+
+let form = document.querySelector('form');
+
+document.getElementById('btnSubmit').addEventListener('click',function(e) {
+    if (email.value == '' || uName.value == ''){
+        alert('Filled Required');
+    }else{
+       setLocalStorage(uName.value, email.value);
+    }
+    e.preventDefault();
+
+    email.value = '';
+    uName.value = '';
+    e.preventDefault();
+})
+
+document.getElementById('fetchData').addEventListener('click',function(e) {
+    console.log('Fetching data: ',localStorage.getItem('Name'), localStorage.getItem('Email'));
+    e.preventDefault();
+});
+
+function setLocalStorage(name, email) {
+    localStorage.setItem("Name", name);
+    localStorage.setItem("Email", email);
+    e.preventDefault();
+}
+
+document.getElementById('removeName').addEventListener('click',function(e) {
+    localStorage.removeItem('Name');
+    e.preventDefault();
+});
+document.getElementById('removeEmail').addEventListener('click',function(e) {
+    localStorage.removeItem('Email');
+    e.preventDefault();
+});
+document.getElementById('clearData').addEventListener('click',function(e) {
+    localStorage.clear();
+});
