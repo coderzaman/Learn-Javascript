779c37d1e0bdcbd164d6a624521553db93709cbc
coderzaman <coder.aktar@gmail.com>
2024-10-05 23:25:13 +0600

    Number Guesser App

--- 24. Project(Shooping Cart)/js/script.js
+++ 24. Project(Shooping Cart)/js/script.js
@@ -1,3 +1,5 @@
+/////////////////////Class/////////////////////
+
 class UI {
     static addItem(Item) {
         let listItem = document.createElement('li');
@@ -5,7 +7,7 @@ class UI {
         listItem.innerHTML = `
                 <p hidden>Product id:<span id="productId">${Item.id}</span></p>
                 <h6 style="display: inline-block;">${Item.name}</h6>
-                <button class="btn btn-danger del_btn">Delete</button>
+                <a href="#" class="btn btn-danger del_btn">Delete</a>
                 <hr>
                 <p style="font-size: 14px;">Price: ${Item.price}</p>
             `
@@ -13,22 +15,19 @@ class UI {
     }
 
     static showAlert(message, className) {
-        let div = document.createElement('div');
-        div.classList = className;
-        div.appendChild(document.createTextNode(message));
-        let parenElement = document.querySelector('.alertMessage');
-        parenElement.insertBefore(div, parenElement.firstChild);
+        errorMessage.classList = `${className} mt-5`;
+        errorMessage.textContent = message;
 
         setTimeout(() => {
-            document.querySelector(`.${className}`).remove();
+            errorMessage.textContent = "";
+            errorMessage.classList = `mt-5`;
         }, 1000)
     }
 
 }
 
-class LocalStorage{
+class LocalStorage {
     static addItemToStorage(id, name, price) {
-
         let cartList;
         let Item = {
             id: id,
@@ -41,15 +40,14 @@ class LocalStorage{
         } else {
             cartList = JSON.parse(localStorage.getItem('cartList'));
         }
-        
+
         cartList.push(Item);
         cartItems.innerText = `${cartList.length}`;
         localStorage.setItem('cartList', JSON.stringify(cartList));
-        UI.showAlert("Added Successfully", 'sucess');
-
+        UI.showAlert("Added Successfully", 'success');
     }
 
-    static getCartList(){
+    static getCartList() {
         let cartList;
         if (localStorage.getItem('cartList') === null) {
             cartList = [];
@@ -57,54 +55,57 @@ class LocalStorage{
             cartList = JSON.parse(localStorage.getItem('cartList'));
         }
         cartItems.innerText = `${cartList.length}`;
-       
+
         cartList.forEach(element => {
             UI.addItem(element);
         });
     }
 
-    static removeItemFromStorage(id){
+    static removeItemFromStorage(id) {
         let cartList = JSON.parse(localStorage.getItem('cartList'));
-  
-        cartList.forEach((item, index)=>{
-             if(item.id == id){
-                cartList.splice(index,1);
-             }
-        })
+
+        for (let i = cartList.length - 1; i >= 0; i--) {
+            if (cartList[i].id == id) {
+                cartList.splice(i, 1);
+                break; 
+            }
+        }
 
         cartItems.innerText = `${cartList.length}`;
         localStorage.setItem('cartList', JSON.stringify(cartList));
-        UI.showAlert("Remove Successfully", 'error');
-
+        UI.showAlert("Removed Successfully", 'error');
     }
 
-    static productIsAlreadyAdded(productId){
-        console.log("Receive Parameter: ", productId);
+    static productIsAlreadyAdded(productId) {
         let cartList = JSON.parse(localStorage.getItem('cartList'));
 
-        if(cartList !==  null){
-           for(let item of cartList){
-             if(item.id == productId){
-                return true;
-             }
-           }
+        if (cartList !== null) {
+            for (let item of cartList) {
+                if (item.id == productId) {
+                    return true;
+                }
+            }
         }
 
         return false;
-
-      
-        
     }
 }
+
+////////////// Get Field, Button and Content ///////////////////
 
 const cart_btn = document.getElementById('cart-btn');
 const cart = document.getElementById('cart');
 const close_cart = document.getElementById('cart_close');
 const all_carts = document.getElementById('all_carts');
 const cart_list = document.getElementById('cart_list');
-const del_cart = document.getElementById('cart_list');
 const cartItems = document.getElementById('number_of_item');
- cart_btn.addEventListener('click', function (e) {
+const errorMessage = document.getElementById('error_message');
+all_carts.addEventListener('click', addToCart);
+cart_list.addEventListener('click', delete_cart);
+document.addEventListener('DOMContentLoaded', LocalStorage.getCartList);
+
+////////////////Event Listner With Function/////////////////////////
+cart_btn.addEventListener('click', function (e) {
     cart.style.display = 'block';
 });
 
@@ -112,37 +113,27 @@ close_cart.addEventListener('click', function (e) {
     cart.style.display = 'none';
 })
 
-all_carts.addEventListener('click', addToCart);
-cart_list.addEventListener('click', delete_cart);
-document.addEventListener('DOMContentLoaded',LocalStorage.getCartList);
-
-
-
-
+/////////////////Function//////////////////
 
 function addToCart(e) {
     if (e.target.hasAttribute('href')) {
         let id = e.target.parentElement.querySelector('#productId').textContent;
         let name = e.target.parentElement.querySelector('#product_name').textContent;
         let price = e.target.parentElement.querySelector('#productPrice').textContent;
-    
- 
-        if(LocalStorage.productIsAlreadyAdded(id)){
+
+        if (LocalStorage.productIsAlreadyAdded(id)) {
             UI.showAlert("Already Added", 'error');
-        }else{
-            UI.addItem({name, price});
+        } else {
+            UI.addItem({ id, name, price }); 
             LocalStorage.addItemToStorage(id, name, price);
         }
-
-     
     }
 }
 
-
-function delete_cart(e){
-    if(e.target.classList.contains('btn-danger')){
+function delete_cart(e) {
+    if (e.target.classList.contains('del_btn')) {
         let id = e.target.parentElement.querySelector('#productId').textContent;
         LocalStorage.removeItemFromStorage(id);
         e.target.parentElement.remove();
-    };
+    }
 }
